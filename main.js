43f5be36722c98aec72d1231576caa77b1214c31
jonathan <97814319+jonathansfx@users.noreply.github.com>
2023-02-10 11:23:06 -0700

    no pop

--- main.js
+++ main.js
@@ -1,17 +1,11 @@
-// array basics
-// array = an ordered collection of data
+let letters = ["a", "b", "c", "d", "e", "f", "g"];
+let nums = [2, 4, 6, 8, 10];
 
-// let evenNums = [2, 4, 6, 10, 12];
-let vowels = ["a", "e", "i", "o", "u"];
-let emptyArray = [];
+letters = arrayInfo(letters);
+nums = arrayInfo(nums);
 
-// access array elements using the index of the element
-let evenNums = [2, 4, 6, 8, 10, 12];
-//    index ->  0  1  2  3  4   5
-
-emptyArray.push("hi");
-console.log(emptyArray);
-emptyArray.push("hello");
-console.log(emptyArray);
-emptyArray.push("hey");
-console.log(emptyArray);
+function arrayInfo(anArray) {
+  console.log(anArray[0]);
+  console.log(anArray.pop());
+  console.log(anArray.length);
+}
