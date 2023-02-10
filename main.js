6876eba33da162bb6f972e065ac8b4e7fd72a043
jonathan <97814319+jonathansfx@users.noreply.github.com>
2023-02-10 10:43:38 -0700

    sdf

---
+++ main.js
@@ -0,0 +1,17 @@
+// array basics
+// array = an ordered collection of data
+
+// let evenNums = [2, 4, 6, 10, 12];
+let vowels = ["a", "e", "i", "o", "u"];
+let emptyArray = [];
+
+// access array elements using the index of the element
+let evenNums = [2, 4, 6, 8, 10, 12];
+//    index ->  0  1  2  3  4   5
+
+emptyArray.push("hi");
+console.log(emptyArray);
+emptyArray.push("hello");
+console.log(emptyArray);
+emptyArray.push("hey");
+console.log(emptyArray);
