36a2652a1f659b1455f74a8dfe7abd6cfeb04aa0
jonathan <97814319+jonathansfx@users.noreply.github.com>
2023-02-13 11:22:18 -0700

    finish

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
-let letters = ["a", "b", "c", "d", "e", "f", "g"];
-let nums = [2, 4, 6, 8, 10];
+// let letters = ["a", "b", "c", "d", "e", "f", "g"];
+// let nums = [2, 4, 6, 8, 10];
 
-letters = arrayInfo(letters);
-nums = arrayInfo(nums);
+// letters = arrayInfo(letters);
+// nums = arrayInfo(nums);
 
-function arrayInfo(anArray) {
-  console.log(anArray[0]);
-  console.log(anArray.pop());
-  console.log(anArray.length);
+// function arrayInfo(anArray) {
+// let x = anArray[anArray.length - 1];
+// console.log(anArray[0]);
+// console.log(x);
+// console.log(anArray.length);
+// }
+
+// arrays with loops
+
+// main algorithm:
+// start with an empty array
+// loop - inside the loop we add elements to the array
+
+let patternArray = [];
+for (let n = 10; n <= 530; n += 20) {
+  patternArray.push(n);
 }
